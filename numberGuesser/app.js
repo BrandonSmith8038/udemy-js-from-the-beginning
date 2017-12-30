c22050a3565e91d5c5d80104ea5e08a13bb15a72
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-30 09:41:27 +0000

    Lose case and game over

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -12,6 +12,9 @@ const game = document.querySelector('#game'),
 			guessInput = document.querySelector('#guess-input'),
 			message = document.querySelector('.message')
 			
+//Clear The Input
+guessInput.value = ''
+			
 //Assign Min and max
 
 minNum.textContent = min;
@@ -24,15 +27,31 @@ guessBtn.addEventListener('click', () => {
 	if(isNaN(guess) || guess < min || guess > max){
 		setMessage(`Please enter a number between ${min} and ${max}`, 'red')
 	}
-	
 	if(guess === winningNum){
-		//Disable Input
-		guessInput.disabled = true
-		guessInput.style.borderColor = 'green'
-		guessInput.style.borderWidth = '2px'
-		setMessage(`${winningNum} IS CORRECT! YOU WIN!!!`, 'green')
-	} else {
+	
+		//Game Over Won
+		gameOver(true,`${winningNum} IS CORRECT! YOU WIN!!!`, 'Play Again')
 		
+	} else {
+		guessesLeft -= 1
+		
+		if(guessesLeft === 0){
+		
+			//Game Over Lost
+			gameOver(false, `GAME OVER, YOU LOST. THE CORRECT NUMBER WAS ${winningNum}`, 'Play Again')
+
+		} else {
+			//Game Continues Answer Wrong
+			
+			//Change Border Color
+			guessInput.style.borderColor = 'red'
+			
+			//Clear The Input
+			guessInput.value = ''
+			
+			//Tell user Its the wrong number
+			setMessage(`GUESS IS NOT CORRECT. ${guessesLeft} GUESSES LEFT`, 'red')
+		}
 	}
 	
 })
@@ -42,3 +61,16 @@ function setMessage(msg, color){
 	message.style.color = color
 	message.textContent = msg
 }
+
+function gameOver(won, msg, btnMsg){
+	
+	let color = won ? 'green' : 'red' 
+	
+	guessInput.disabled = true
+	guessInput.style.borderColor = color
+	guessInput.style.borderWidth = '2px'
+	setMessage(msg, color)
+	guessBtn.setAttribute('value', btnMsg)
+	guessInput.value = ''
+}
+
