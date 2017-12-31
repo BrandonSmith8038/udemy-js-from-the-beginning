6375956dddc4d9e5bd45e59689f62f2918b15f92
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-31 13:56:52 +0000

    Post Request

--- easyHTTP/app.js
+++ easyHTTP/app.js
@@ -1,12 +1,38 @@
 const http = new easyHTTP;
 
-//Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
+// //Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
   
-  if(err){
-      console.log(err)
-  } else {
-    console.log(response)  
-  }
+//   if(err){
+//       console.log(err)
+//   } else {
+//     console.log(response)  
+//   }
   
+// })
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+  
+//   if(err){
+//       console.log(err)
+//   } else {
+//     console.log(response)  
+//   }
+  
+// })
+
+//Create Data
+const data = {
+    "title": "Custom Post",
+    "body": "This is a custom post"
+}
+
+//Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, response) {
+    if(err){
+        console.log(err)
+    } else {
+        console.log(response)
+    }
 })
--- easyHTTP/easyHTTP.js
+++ easyHTTP/easyHTTP.js
@@ -1,29 +1,44 @@
 function easyHTTP() {
-    this.http = new XMLHttpRequest()
+	this.http = new XMLHttpRequest()
 }
 
 //Make Get Request
 
 easyHTTP.prototype.get = function(url, callback){
-    this.http.open('GET', url, true)
-    
-    let self = this
-    
-    this.http.onload = function(){
-    
-        if(self.http.status === 200){
-            callback(null, self.http.responseText)
-        } else {
-            callback('Error: ' + self.http.status)
-        }
-    }
-    
-    this.http.send()
+	this.http.open('GET', url, true)
+	
+	let self = this
+	
+	this.http.onload = function(){
+	
+		if(self.http.status === 200){
+			callback(null, self.http.responseText)
+		} else {
+			callback('Error: ' + self.http.status)
+		}
+	}
+	
+	this.http.send()
 }
 
 
 
 //Make Post Request
+
+easyHTTP.prototype.post = function(url, data, callback){
+	this.http.open('POST', url, true)
+	
+	this.http.setRequestHeader('Content-type', 'application/json')
+	
+	const self = this
+	
+	this.http.onload = function(){
+		
+		callback(null, self.http.responseText)
+	}
+	
+	this.http.send(JSON.stringify(data))
+}
 
 //Make Delete Request
 
