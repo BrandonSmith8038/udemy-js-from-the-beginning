2af14dca60d80971e1c960dc70719060ef82c65b
Brandon Smith <brandon@reddirtwebdesign.com>
2018-01-11 13:27:41 +0000

    Controllers and Data Structures

---
+++ tracalorie/app.js
@@ -0,0 +1,58 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+ //Item Constructor
+ const Item = function(id, name, calories){
+   this.id = id
+   this.name = name
+   this.calories = calories
+ }
+ 
+ // Data Structure / State
+ const data = {
+   items: [
+     {
+       id: 0,
+       name: 'Steak Dinner',
+       calories: 1200
+     },
+     {
+       id: 1,
+       name: 'Cookie',
+       calories: 400
+     },
+     {
+       id: 2,
+       name: 'Eggs',
+       calories: 200
+     },
+     ],
+   currentItem: null,
+   totalCalories: 0
+ }
+ return {
+   logData: function(){
+     return data
+   }
+ }
+})()
+
+
+// UI Controller
+const UICtrl = (function() {
+
+})()
+
+// App Controller
+const AppCtrl = (function(ItemCtrl, UICtrl) {
+  return {
+    init: function(){
+      console.log('Initalizing App...')
+    }
+  }
+})(ItemCtrl, UICtrl)
+
+
+// Initializing App
+  AppCtrl.init()
