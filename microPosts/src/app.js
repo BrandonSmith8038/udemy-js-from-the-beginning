1abbe5647dd816e4c8a989fc6e3ee9f390f37cbc
Brandon Smith <brandon@reddirtwebdesign.com>
2018-01-13 09:56:11 -0700

    Deleteing Posts

--- microPosts/src/app.js
+++ microPosts/src/app.js
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', getPosts);
 //Listen For Submit Post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+//Listen For Delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+//List for edit state
+document.querySelector('#posts').addEventListener('click', editPosts);
+
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
@@ -34,3 +40,18 @@ function submitPost() {
     })
     .catch(err => console.log(err));
 }
+function deletePost(e) {
+  if (e.target.parentElement.classList.contains('delete')) {
+    console.log(e.target.parentElement.dataset);
+    const id = e.target.parentElement.dataset.id;
+    console.log(id);
+    http
+      .delete(`http://localhost:3000/posts/${id}`)
+      .then(message => {
+        ui.showAlert(message, 'alert alert-danger');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+  }
+}
+function editPosts(e) {}
